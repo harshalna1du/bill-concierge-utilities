f5ec663f81bb10b102c3fa0406e804a5a7694ad3
Harshal Naidu <hnaidu@paymentus.com>
2025-07-19 19:16:43 -0400

    Combined Baseline 2.1

--- pdf/parsePdf.js
+++ pdf/parsePdf.js
@@ -2,4 +2,5 @@
  * @fileoverview Placeholder for PDF parsing utilities.
  * This module will contain functions to extract text and other data from PDF files.
  * To be implemented.
+ * This is a future plan. 
  */
